1d45033c5c8a0591ae8ea285649d1ce30dced0db
Ryan <73148864+Ryan-000@users.noreply.github.com>
2025-01-30 18:11:22 -0500

    Optimize
    Small optimization

--- servers/rendering/rendering_device.cpp
+++ servers/rendering/rendering_device.cpp
@@ -6421,7 +6421,9 @@ void RenderingDevice::_begin_frame(bool p_presented) {
 	}
 
 	if (p_presented) {
-		update_perf_report();
+		if (GLOBAL_GET("optimizations/enable_perf_report")) {
+			update_perf_report();
+		}
 		driver->linear_uniform_set_pools_reset(frame);
 	}
 
@@ -8111,6 +8113,7 @@ RenderingDevice::RenderingDevice() {
 	}
 
 	render_thread_id = Thread::get_caller_id();
+	GLOBAL_DEF("optimizations/enable_perf_report", false);
 }
 
 /*****************/
